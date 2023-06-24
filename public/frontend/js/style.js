2d2f2099ca94e14ea7000d84f084708fe1fa5ea1
Tu Mai Thien <tumaithien@gmail.com>
2023-06-24 22:10:31 +0700

    ui video course

--- public/frontend/js/style.js
+++ public/frontend/js/style.js
@@ -146,3 +146,13 @@ $(function () {
     }
   });
 });
+
+on("click", ".videoCourse_collapsible", function (e) {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+});
