e6e10b1bd7b75d0e59238193759b69c4e58c19ce
danganh <danganh.hna@gmail.com>
2023-06-25 01:13:00 +0700

    update document

--- apps/repositories/Document.php
+++ apps/repositories/Document.php
@@ -11,30 +11,48 @@ use \Learncom\Models\LearnArticle;
 
 class Document extends Component
 {
-    public static function findFirstById($id) {
+    public static function findFirstById($id)
+    {
         return LearnDocument::findFirst([
             'document_id = :id:',
             'bind' => ['id' => $id]
         ]);
     }
 
-    public static function findByClassAndSubjectAndChapter($class_id,$subject_id,$chapter_id,$type = "normal") {
+    public static function findByClassAndSubjectAndChapter($class_id, $subject_id, $chapter_id, $type = "normal")
+    {
         return LearnDocument::find([
             'document_class_id = :class_id: AND document_chapter_id = :chapter_id: AND document_type = :type: 
             AND document_subject_id = :subject: AND document_active = "Y" ',
-            'bind' => ['class_id' => $class_id,'subject'=>$subject_id, 'chapter_id' => $chapter_id, 'type' => $type],
+            'bind' => ['class_id' => $class_id, 'subject' => $subject_id, 'chapter_id' => $chapter_id, 'type' => $type],
             'order' => 'document_order ASC'
         ]);
     }
-    public static function findByClassAndSubjectAndChapterAndGroup($class_id,$subject_id,$chapter_id,$group_id,$type = "normal") {
+    public static function findByClassAndSubjectAndChapterAndCache($class_id, $subject_id, $chapter_id, $type = "normal")
+    {
+        $cache = new CacheRepo("document_findByClassAndSubjectAndChapterAndCache_{$class_id}_{$subject_id}_{$chapter_id}_{$type}");
+        $data = $cache->getCache();
+        if (!$data) {
+            $data = LearnDocument::find([
+                'document_class_id = :class_id: AND document_chapter_id = :chapter_id: AND document_type = :type: 
+                AND document_subject_id = :subject: AND document_active = "Y" ',
+                'bind' => ['class_id' => $class_id, 'subject' => $subject_id, 'chapter_id' => $chapter_id, 'type' => $type],
+                'order' => 'document_order ASC'
+            ])->toArray();
+            $cache->setCache($data);
+        }
+        return $data;
+    }
+    public static function findByClassAndSubjectAndChapterAndGroup($class_id, $subject_id, $chapter_id, $group_id, $type = "normal")
+    {
         return LearnDocument::find([
-            'document_class_id = :class_id: AND document_chapter_id = :chapter_id: AND document_type = :type: AND (FIND_IN_SET(:group_id:,document_group_ids) OR document_group_ids = "" OR document_group_ids is NULL)
+            'document_class_id = :class_id:
+             AND document_chapter_id = :chapter_id: 
+             AND document_type = :type: 
+             AND (FIND_IN_SET(:group_id:,document_group_ids) OR document_group_ids = "" OR document_group_ids is NULL)
             AND document_subject_id = :subject: AND document_active = "Y" ',
-            'bind' => ['class_id' => $class_id,'subject'=>$subject_id, 'chapter_id' => $chapter_id, 'type' => $type,'group_id' => $group_id],
+            'bind' => ['class_id' => $class_id, 'subject' => $subject_id, 'chapter_id' => $chapter_id, 'type' => $type, 'group_id' => $group_id],
             'order' => 'document_order ASC'
         ]);
     }
 }
-
-
-
