6cbf0d562165c09d0b6fe7fac8b87728787ab859
danganh <danganh.hna@gmail.com>
2023-07-20 23:32:42 +0700

    fix small

--- apps/frontend/controllers/DgnlController.php
+++ apps/frontend/controllers/DgnlController.php
@@ -33,8 +33,7 @@ class DgnlController extends ControllerBase
     public function viewAction()
     {
         $this->dgnl_select = $this->dispatcher->getParam('type_id');
-        var_dump($this->dgnl_select);exit;
-        $this->video_id = $this->request->get('video_id');
+        $video_id = $this->request->get('video_id');
 
         if (!in_array($this->dgnl_select, $this->dgnl_type_id)) {
             return $this->response->redirect("/permission");
@@ -44,6 +43,24 @@ class DgnlController extends ControllerBase
         $this->type = "video";
         $repoPage = new Page();
         $repoPage->AutoGenMetaPage('khoahoc', 'Course');
+
+        $result = $this->formatToCouse($video_id);
+        if (empty($result)) {
+            $this->view->setVars([
+                'link' => "",
+                'chapters' => [],
+                'lessons' => [],
+                'video_selected' => $video_id,
+            ]);
+        } else {
+            $this->view->setVars($result);
+        }
+
+        $this->view->pick("course/index");
+
+    }
+    public function formatToCouse($video_id)
+    {
         $arDgnlVideo = LearnDgnl::find([
             'dgnl_type_id = :type:',
             'bind' => [
@@ -51,14 +68,54 @@ class DgnlController extends ControllerBase
             ],
             'order' => "learn_category ASC,dgnl_order ASC"
         ])->toArray();
-        var_dump($arDgnlVideo);exit;
+        $dgnlModel = [];
+        if ($video_id) {
+            $dgnlModel = array_filter($arDgnlVideo, function ($dgnl) use ($video_id) {
+                return $dgnl['dgnl_id'] == $video_id;
+            }, ARRAY_FILTER_USE_BOTH);
+            $dgnlModel = reset($dgnlModel);
 
-        $this->view->setVars([
-            // 'type' => $this->type,
-            'parent_keyword' => $parent_keyword,
-            'arDgnlVideo' => $arDgnlVideo,
-            'video_selected' => $this->video_id,
-        ]);
+        }
+        if (empty($dgnlModel)) {
+            $dgnlModel = $arDgnlVideo[0]['dgnl_link'] ?? [];
+        }
+
+        $chapters = [];
+        $lessons = [];
+        if (empty($dgnlModel)) {
+            return [];
+        }
+        $link = $dgnlModel['dgnl_link'] ?? "";
+        //format lại: chapter: video/bài giảng, lessons: list video
+        $chapters = [
+            'video' => [
+                'id' => 'video',
+                'name' => "Video",
+                'active' => $dgnlModel['learn_category'] == "video" ? "active" : ""
+            ],
+            'khoahoc' => [
+                'id' => 'khoahoc',
+                'name' => "Bài Giảng",
+                'active' => $dgnlModel['learn_category'] == "khoahoc" ? "active" : ""
+            ]
+        ];
+        foreach ($arDgnlVideo as $video) {
+            $active_lesson = $video['dgnl_id'] == $video_id ? "active" : "";
+            $lessons[$video['learn_category']][] = [
+                'id' => $video['dgnl_id'],
+                'name' => $video['dgnl_name'],
+                'link' => $video['dgnl_link'],
+                'active' => $active_lesson,
+                'href' => "/dgnl/{$video['dgnl_type_id']}?video_id={$video['dgnl_id']}"
+            ];
+        }
+        return [
+            'link' => $link,
+            'chapters' => $chapters,
+            'lessons' => $lessons,
+            'video_selected' => $video_id,
+        ];
+
     }
 
 }
